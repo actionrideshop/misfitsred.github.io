cfbeeecede624889ec3c40f81a08e1a2c41b0d8f
Hicks-Brennan <55519908+Hicks-Brennan@users.noreply.github.com>
2020-05-04 18:13:23 -0700

    First Commit

---
+++ js/countdownClock.js
@@ -0,0 +1,34 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Sept 09, 2020 18:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("days").innerHTML = days + " Days";
+  document.getElementById("hours").innerHTML = hours + " Hours";
+  document.getElementById("minutes").innerHTML = minutes + " Minutes";
+  //document.getElementById("seconds").innerHTML = seconds + " Seconds"
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+function toggleMenu() {
+  document.getElementsByClassName("navList")[0].classList.toggle("responsive");
+}
